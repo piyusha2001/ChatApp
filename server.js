b309b35ff87bac963cac48d7735b4806ea009384
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-14 19:47:00 +0530

    console logging message after connection

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const http = require("http");
 const socketio = require("socket.io");
 
 const app = express();
-const io = socketio(server);
 const PORT = 3000 || process.env.PORT;
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 io.on("connection", (socket) => {
-    console.log("New wb connection");
+    console.log("New ws connection");
+    socket.emit("message", "Wecome to chatcord");
 });
 
 server.listen(PORT, () => console.log("Server running on port 3000"));
