6f0d32389469232a86971382ae0e18e08b5771d4
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-14 20:56:13 +0530

    added event listener to submit message

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
+const chatForm = document.getElementById("chat-form");
 const socket = io();
 
 socket.on("message", (message) => {
     console.log(message);
+    outputMessage(message);
 });
+
+chatForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+    socket.emit("chatMessage", msg);
+});
+
+//output message to Dom
+
+function outputMessage(message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+}
