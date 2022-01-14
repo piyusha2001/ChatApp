fc82629cfddd5d1a69be2892eae83771dcccbeb2
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-15 00:34:52 +0530

    join room functionality added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-
+const { userJoin, getCurrentUser } = require("./utils/users");
 const app = express();
 const PORT = 3000 || process.env.PORT;
 const server = http.createServer(app);
@@ -13,21 +13,27 @@ app.use(express.static(path.join(__dirname, "public")));
 const botName = "J.A.R.V.I.S(Bot) ";
 
 io.on("connection", (socket) => {
-    socket.emit("message", formatMessage(botName, "Welcome to chatcord"));
+    socket.on("joinRoom", ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+        //Welcome message
+        socket.emit("message", formatMessage(botName, "Welcome to chatcord"));
 
-    //Broadcast when user connects
-    socket.broadcast.emit(
-        "message",
-        formatMessage(botName, "A user has entered the chat")
-    );
-
-    //Runs when client disconnects
-    socket.on("disconnect", () => {
-        io.emit("message", formatMessage(botName, "A user has left the chat"));
+        //Broadcast when user connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                "message",
+                formatMessage(botName, ` ${user.username} has entered the chat`)
+            );
     });
 
     socket.on("chatMessage", (msg) => {
         io.emit("message", formatMessage("USER ", msg));
+    });
+    //Runs when client disconnects
+    socket.on("disconnect", () => {
+        io.emit("message", formatMessage(botName, "A user has left the chat"));
     });
 });
 
