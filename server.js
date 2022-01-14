a03f29fddfb6f31c32d0e05adaddf7ec5042fcbf
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-14 23:04:20 +0530

    passed arguments to formatmessage func

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -9,19 +10,24 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+const botName = "J.A.R.V.I.S(Bot) ";
+
 io.on("connection", (socket) => {
-    socket.emit("message", "Wecome to chatcord");
+    socket.emit("message", formatMessage(botName, "Welcome to chatcord"));
 
     //Broadcast when user connects
-    socket.broadcast.emit("message", "A user has entered the chat");
+    socket.broadcast.emit(
+        "message",
+        formatMessage(botName, "A user has entered the chat")
+    );
 
     //Runs when client disconnects
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left the chat");
+        io.emit("message", formatMessage(botName, "A user has left the chat"));
     });
 
     socket.on("chatMessage", (msg) => {
-        io.emit("message", msg);
+        io.emit("message", formatMessage("USER ", msg));
     });
 });
 
