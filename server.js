7705996839d01b984da220b810e95a61f1da1297
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-14 20:55:21 +0530

    added disconnect and sent msg to client side

--- server.js
+++ server.js
@@ -10,8 +10,19 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 io.on("connection", (socket) => {
-    console.log("New ws connection");
     socket.emit("message", "Wecome to chatcord");
+
+    //Broadcast when user connects
+    socket.broadcast.emit("message", "A user has entered the chat");
+
+    //Runs when client disconnects
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat");
+    });
+
+    socket.on("chatMessage", (msg) => {
+        io.emit("message", msg);
+    });
 });
 
 server.listen(PORT, () => console.log("Server running on port 3000"));
