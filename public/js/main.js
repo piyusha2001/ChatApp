342ce3fe3660bc1e67d528e6fcb6145d5f45dbf0
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-14 23:47:34 +0530

    sent username and room to server

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
 const socket = io();
+
+//Join chatroom
+socket.emit("joinRoom", { username, room });
 
 socket.on("message", (message) => {
     console.log(message);
