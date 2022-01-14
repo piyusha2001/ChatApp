5b3f00b72ba4fb236295d734490728bf3bd81763
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-14 19:18:00 +0530

    socket.io initialised

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = 3000 || process.env.PORT;
-app.use(express.static(path.join(__dirname, "public")));
+const http = require("http");
+const socketio = require("socket.io");
 
-app.listen(PORT, () => console.log("Server running on port ${PORT}"));
+const app = express();
+const io = socketio(server);
+const PORT = 3000 || process.env.PORT;
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "public")));
+io.on("connection", (socket) => {
+    console.log("New wb connection");
+});
+
+server.listen(PORT, () => console.log("Server running on port 3000"));
