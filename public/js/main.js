bce726975f44d3270815d912d09c112b22fa814c
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-19 01:14:31 +0530

    Room added to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,51 @@
-const chatForm = document.getElementById("chat-form");
-const chatMessages = document.querySelector(".chat-messages");
-
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 const { username, room } = Qs.parse(location.search, {
-    ignoreQueryPrefix: true,
+	ignoreQueryPrefix: true,
 });
 const socket = io();
 
 //Join chatroom
-socket.emit("joinRoom", { username, room });
+socket.emit('joinRoom', { username, room });
 
-socket.on("message", (message) => {
-    console.log(message);
-    outputMessage(message);
-
-    //scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+//Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+	outputRoomName(room);
+	outputUsers(users);
 });
 
-chatForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const msg = e.target.elements.msg.value;
-    socket.emit("chatMessage", msg);
-    e.target.elements.msg.value = "";
-    e.target.elements.msg.focus();
+//Message from server
+socket.on('message', (message) => {
+	console.log(message);
+	outputMessage(message);
+
+	//scroll down
+	chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const msg = e.target.elements.msg.value;
+	socket.emit('chatMessage', msg);
+	e.target.elements.msg.value = '';
+	e.target.elements.msg.focus();
 });
 
 //output message to Dom
 
 function outputMessage(message) {
-    const div = document.createElement("div");
-    div.classList.add("message");
-    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
+	const div = document.createElement('div');
+	div.classList.add('message');
+	div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
                         <p class="text">
                             ${message.text}
                         </p>`;
-    document.querySelector(".chat-messages").appendChild(div);
+	document.querySelector('.chat-messages').appendChild(div);
+}
+
+//Add room name to DOM
+function outputRoomName(room) {
+	roomName.innerText = room;
 }
