586304a09fce2cd5c2bafb28cdb5b0beceb67a72
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-19 00:47:13 +0530

    function added for user leave

--- utils/users.js
+++ utils/users.js
@@ -2,14 +2,22 @@ const users = [];
 
 //Join user to chat
 function userJoin(id, username, room) {
-    const user = { id, username, room };
-    users.push(user);
-    return user;
+	const user = { id, username, room };
+	users.push(user);
+	return user;
 }
 
 //Get current user
 function getCurrentUser(id) {
-    return users.find((user) => user.id === id);
+	return users.find((user) => user.id === id);
 }
 
-module.exports = { userJoin, getCurrentUser };
+//User leaves chat
+function userLeave(id) {
+	const index = users.findIndex((user) => user.id === id);
+	if (index !== -1) {
+		return users.splice(index, 1);
+	}
+}
+
+module.exports = { userJoin, getCurrentUser, userLeave };
